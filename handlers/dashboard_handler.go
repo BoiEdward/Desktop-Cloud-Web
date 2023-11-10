708482d8c9179d942a610da359e6fcebfd8165c1
Jhostil <jhooiineer@gmail.com>
2023-11-09 19:15:02 -0500

    Se modifica el còdigo del dashboard para mostrar mètricas reales de la plataforma

--- handlers/dashboard_handler.go
+++ handlers/dashboard_handler.go
@@ -1,36 +1,55 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 // DatosCatalogo representa los datos para el catálogo de máquinas virtuales
-type DatosCatalogo struct {
-	TotalMaquinas       int // Total de máquinas virtuales
-	TotalEncendidas     int
-	UsuariosRegistrados int // Total de usuarios registrados
-	TotalCPU            int // Total de CPU utilizado
-	TotalRAM            int // Total de RAM utilizada
+type DatosDashboard struct {
+	Total_maquinas_creadas    int
+	Total_maquinas_encendidas int
+	Total_usuarios            int
+	Total_estudiantes         int
+	Total_invitados           int
+	Total_RAM                 int
+	Total_RAM_usada           int
+	Total_CPU                 int
+	Total_CPU_usada           int
 }
 
 func DashboardHandler(c *gin.Context) {
-	// ... tu código existente ...
 
 	// Calcula los datos para el catálogo (esto es solo un ejemplo, debes obtener estos datos de tu lógica)
-	datosCatalogo := DatosCatalogo{
-		TotalMaquinas:       100,
-		TotalEncendidas:     25,
-		UsuariosRegistrados: 50,
-		TotalCPU:            75,
-		TotalRAM:            200,
-	}
+	datosDashboard, _ := consultarMetricas()
 
 	c.HTML(http.StatusOK, "dashboard.html", gin.H{
 		"email":          "email",
 		"machines":       nil,
 		"machinesChange": nil,
-		"datosCatalogo":  datosCatalogo,
+		"datosDashboard": datosDashboard,
 	})
+}
+
+func consultarMetricas() (DatosDashboard, error) {
+	var metricas DatosDashboard
+
+	resp, err := http.Get("http://localhost:8081/json/consultMetrics")
+	if err != nil {
+		return metricas, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return metricas, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&metricas)
+	if err != nil {
+		return metricas, err
+	}
+
+	return metricas, nil
 }
